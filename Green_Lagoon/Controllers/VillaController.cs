f43d9ac5a4b01d20d215bea3e2ee5b659feaa6bb
String101 <sbongakonkesihle31@gmail.com>
2023-12-02 12:59:29 +0200

    1st faild Database

---
+++ Green_Lagoon/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Green_Lagoon.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Green_Lagoon.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villa=_context.Villas.ToList();
+            return View();
+        }
+    }
+}
