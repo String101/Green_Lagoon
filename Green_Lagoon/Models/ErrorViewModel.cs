650b7f500c7604b5d214db23b6296bbc11826997
String101 <sbongakonkesihle31@gmail.com>
2023-12-02 08:09:15 +0200

    Add project files.

---
+++ Green_Lagoon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Green_Lagoon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
