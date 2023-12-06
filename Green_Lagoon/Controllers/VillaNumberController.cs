64e4f613ee4022d7f24dfe1ebdf75ea7abfe3305
String101 <sbongakonkesihle31@gmail.com>
2023-12-06 18:18:57 +0200

    VillaNumber_Create and Udate

--- Green_Lagoon/Controllers/VillaNumberController.cs
+++ Green_Lagoon/Controllers/VillaNumberController.cs
@@ -1,7 +1,9 @@
 ﻿using Green_Lagoon.Domain.Entities;
 using Green_Lagoon.Infrastructure.Data;
+using Green_Lagoon.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Green_Lagoon.Controllers
 {
@@ -15,43 +17,65 @@ namespace Green_Lagoon.Controllers
         } 
         public IActionResult Index()
         {
-            var villaNumbers=_context.VillaNumbers.ToList();
+            var villaNumbers=_context.VillaNumbers.Include(u=>u.Villa).ToList();
             return View(villaNumbers);
         }
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> list = _context.Villas.ToList().Select(u=>new SelectListItem 
+            VillaNumberViewModel villaNumber = new()
             {
-                Text = u.Name,
-                Value = u.Id.ToString(),
-            });
-            ViewData["Villalist"] = list;
-            return View();
+                VillaList=_context.Villas.Select(u=> new SelectListItem
+                {
+                    Text = u.Name,
+                    Value= u.Id.ToString(),
+                })
+            };
+            
+            
+            return View(villaNumber);
         }
         [HttpPost]
-        public IActionResult Create(VillaNumber obj)
+        public IActionResult Create(VillaNumberViewModel obj)
         {
-           
-            if(ModelState.IsValid)
+            bool roomNumberExists = _context.VillaNumbers.Any(u => u.Villa_Number == obj.VillaNumber.Villa_Number);
+            ModelState.Remove("VillaList");
+            if(ModelState.IsValid && !roomNumberExists)
             {
-                _context.VillaNumbers.Add(obj);
+                _context.VillaNumbers.Add(obj.VillaNumber);
                 _context.SaveChanges();
                 TempData["success"] = "The Villa Number has been created successfully.";
                 return RedirectToAction("Index");
             }
-            TempData["error"] = "The Villa Number can not be created.";
-            return View();
+            if (roomNumberExists)
+            {
+                TempData["error"] = "The Villa Number already exists";
+            }
+           obj.VillaList= _context.Villas.Select(u => new SelectListItem
+           {
+               Text = u.Name,
+               Value = u.Id.ToString(),
+           });
+            return View(obj);
             
         }
         [HttpGet]
-        public IActionResult Update(int villaid)
+        public IActionResult Update(int villaNumberId)
         {
-            Villa? obj= _context.Villas.FirstOrDefault(x => x.Id==villaid);
-            if(obj==null) 
+            VillaNumberViewModel villaNumberVM = new()
+            {
+                VillaList = _context.Villas.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                VillaNumber = _context.VillaNumbers.FirstOrDefault(u=>u.Villa_Number==villaNumberId)
+            };
+           
+            if(villaNumberVM.VillaNumber==null) 
             { 
                return RedirectToAction("Error","Home");
             }
-            return View(obj);
+            return View(villaNumberVM);
         }
         [HttpPost]
         public IActionResult Update(Villa obj)
---
+++ Green_Lagoon/ViewModels/VillaNumberViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Green_Lagoon.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Green_Lagoon.ViewModels
+{
+    public class VillaNumberViewModel
+    {
+        public VillaNumber VillaNumber { get; set; }
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
